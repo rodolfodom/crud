aa041db09128c35746bab922c6662dd66b937b8f
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2020-12-20 09:12:58 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const d = document,
+  $tabla = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.querySelector(".crud-template"),
+  $fragment = d.createDocumentFragment();
